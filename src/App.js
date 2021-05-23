1805463f4623fa1ba4c8e2156d2b53996f88b8e7
Elyas <elyassalar8@gmail.com>
2021-05-23 21:31:25 +0300

    upload entire thestarter E-commerce project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Homepage from './components/homepage.component';
+export default class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Homepage />
+            </div>
+        )
+    }
+}
