86cba61b6743608e29e733aae4b32d480a88287a
Elyas <elyassalar8@gmail.com>
2021-05-28 13:37:28 +0300

    adding header section for each page, adding it on top of the routes to run regardless to the path.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import ShopPage from './pages/shop/shop.component';
 import {Route, Switch} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
+import Header from './components/header/header.component';
+
 export default class App extends React.Component{
     render(){
         return(
             <div>
+                <Header />
                 <Switch>
                     <Route exact path='/' component={Homepage} />
                     <Route exact path='/shop' component={ShopPage} />
